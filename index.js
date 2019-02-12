8c3df6ae6c926d92dceadd4f8ee0ba37b54f1613
javad <javadbat@gmail.com>
2019-02-12 16:19:45 +0330

    load cjs as defualt

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 (function(process) {
   'use strict';
   if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./dist/Router.min');
+    module.exports = require('./dist/Router.cjs.min');
   } else {
-    module.exports = require('./dist/Router.min');
+    module.exports = require('./dist/Router.cjs.min');
   }
 })(require('process'));
